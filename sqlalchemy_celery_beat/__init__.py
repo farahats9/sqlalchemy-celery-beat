570f6f7b832c0aae54f5d062a6d61ddbff814f51
Mohamed Farahat <farahats9@yahoo.com>
2024-06-11 23:12:56 +0300

    bumpt version for release

--- sqlalchemy_celery_beat/__init__.py
+++ sqlalchemy_celery_beat/__init__.py
@@ -9,7 +9,7 @@ from .models import (
 )
 from .schedulers import DatabaseScheduler
 
-__version__ = '0.7.1'
+__version__ = '0.8.0'
 __author__ = 'Mohamed Farahat'
 __contact__ = 'farahats9@yahoo.com'
 __homepage__ = 'https://github.com/farahats9/sqlalchemy-celery-beat'
